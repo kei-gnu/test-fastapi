8e732850cf44de9a0cf203b00f1a5ef22558229a
tomita <319012kei@users.noreply.github.com>
2022-05-07 22:57:46 +0900

    Jinja2 settings

--- controllers.py
+++ controllers.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.templating import Jinja2Templates
 from starlette.requests import Request
 
 app = FastAPI(
@@ -6,6 +7,11 @@ app = FastAPI(
     description="FastAPI tutorial: Let's make a simple toDo Application with FastAPI(and starlette)",
     version='1.0 beta test'
 )
+
+# new template related settings(jinja2)
+templates = Jinja2Templates(directory="templates_test")
+# Jinja2.Environment : settings for filter and global
+jinja_env = templates.env    
 
 def index_test(request: Request):
     return {'Hello': 'World!!'}
