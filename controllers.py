8b5af2fcb356b6dd9c2dce59c204cb6443c3ce39
tomita <319012kei@users.noreply.github.com>
2022-05-07 22:39:43 +0900

    update file

---
+++ controllers.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from starlette.requests import Request
+
+app = FastAPI(
+    title="I make a toDo Application with FastAPI",
+    description="FastAPI tutorial: Let's make a simple toDo Application with FastAPI(and starlette)",
+    version='1.0 beta test'
+)
+
+def index(request: Request):
+    return {'Hello': 'World!!'}
